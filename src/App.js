675feb6cf75362124fc720d5f3ed60cc6e4eaede
HostVanyaD <reeedruuum@gmail.com>
2022-07-19 11:36:01 +0300

    Add category-item and categories components+styles

--- src/App.js
+++ src/App.js
@@ -1,42 +1,7 @@
-import './categories.styles.scss';
+import Categories from './components/categories/categories.component';
 
 const App = () => {
-  const ctagries = [
-    {
-      id: 1,
-      title: 'Hats',
-    },
-    {
-      id: 2,
-      title: 'Jackets',
-    },
-    {
-      id: 3,
-      title: 'Sneakers',
-    },
-    {
-      id: 4,
-      title: 'Womens',
-    },
-    {
-      id: 5,
-      title: 'Mens',
-    },
-  ];
-
-  return (
-    <div className='categories-container'>
-      {ctagries.map(({ id, title }) => (
-        <div key={id} className='category-container'>
-          <div className='category-body'>
-            {<img />}
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <Categories />;
 };
 
 export default App;
